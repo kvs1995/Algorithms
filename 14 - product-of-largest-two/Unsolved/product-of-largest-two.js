2dc73ce73f53631d16b384b038d4fb1f593e7dcc
Kendall Swanson <42819374+kvs1995@users.noreply.github.com>
2022-04-26 03:41:57 -0500

    on fire - completed product of two largest numbers

--- 14 - product-of-largest-two/Unsolved/product-of-largest-two.js
+++ 14 - product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,3 +1,14 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-var productOfLargestTwo = function(arr) {};
+var productOfLargestTwo = function(arr) {
+  let newArray = [];
+  const max1 = Math.max(...arr)
+  const max1Index = arr.indexOf(max1)
+  arr.map(num => {
+    if (arr.indexOf(num) !== max1Index) {
+      newArray.push(num)
+    }
+  })
+  const max2 = Math.max(...newArray)
+  return max1*max2
+};
