2cae019e8b900ed3da588c9c64e9e08a60355629
Kendall Swanson <42819374+kvs1995@users.noreply.github.com>
2022-04-26 03:58:16 -0500

    camel case action

--- 15 - camel-case/Unsolved/camel-case.js
+++ 15 - camel-case/Unsolved/camel-case.js
@@ -1,3 +1,12 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {};
+var camelCase = function(str) {
+  let listString = []
+  listString.push(str[0].toLowerCase())
+  for (let i=1; i < str.length; i++) {
+    str[i-1] === " " ? listString.push(str[i].toUpperCase())
+    : str[i] === " " ? console.log("Oooh a space")
+    : listString.push(str[i])
+  }
+  return listString.join("")
+};
