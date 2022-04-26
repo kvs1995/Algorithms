33676cc7e3306dfca9b5430bfaaa1732cc9c5a0b
Kendall Swanson <42819374+kvs1995@users.noreply.github.com>
2022-04-26 04:45:57 -0500

    is anagram was a wee bit harder . go over in the morning

--- 18 - is-anagram/Unsolved/is-anagram.js
+++ 18 - is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,25 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
-var isAnagram = function(strA, strB) {};
+var isAnagram = function(strA, strB) {
+  if (strA.length !== strB.length) {
+    return false
+  } else {
+    let strACount = {}
+    let sum =0;
+    for (var i = 0; i < strA.length; i++) {
+      !strACount[strA[i]] ? strACount[strA[i]] = 1 : strACount[strA[i]] += 1
+
+      !strACount[strB[i]] ? strACount[strB[i]] = -1 :strACount[strB[i]] = strACount[strB[i]] - 1
+    }
+    for (const key in strACount) {
+      if (strACount[key] !==0) {
+        return false
+      } else {
+        sum+=strACount[key]
+      }
+    }
+    const result = sum === 0 ? true : false
+    return result
+  }
+};
