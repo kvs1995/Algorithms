dfdc950dc3f62cf3f8451f4540ad8dc6c29caf94
Kendall Swanson <42819374+kvs1995@users.noreply.github.com>
2022-04-26 03:17:36 -0500

    factorials lets go

--- 12 - factorial/Unsolved/factorial.js
+++ 12 - factorial/Unsolved/factorial.js
@@ -1,3 +1,16 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+  let result = 1
+  let count = 1
+
+  if (num === 0) {
+    return 1
+  } else {
+    while (count <= num) {
+      result = result * count
+      count ++;
+    }
+    return result;
+  }
+};
