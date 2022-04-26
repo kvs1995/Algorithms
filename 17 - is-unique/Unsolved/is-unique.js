1e000db92ad522b0423f12023a776e7bbb8561e8
Kendall Swanson <42819374+kvs1995@users.noreply.github.com>
2022-04-26 04:16:45 -0500

    coding coding coding - finished is Unique

--- 17 - is-unique/Unsolved/is-unique.js
+++ 17 - is-unique/Unsolved/is-unique.js
@@ -1,4 +1,12 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function(arr) {
+  let numCount = {}
+  let isItUnique = true
+  for (let i=0 ; i < arr.length && isItUnique; i++) {
+    !numCount[arr[i]] ? numCount[arr[i]] = 1
+    : isItUnique = false
+  }
+  return isItUnique
+};
