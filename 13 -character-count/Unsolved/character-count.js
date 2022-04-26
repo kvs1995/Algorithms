13e9de479a7ae56414de2ce1fff0ea831ee56536
Kendall Swanson <42819374+kvs1995@users.noreply.github.com>
2022-04-26 03:27:25 -0500

    character count letsa go uno mas

--- 13 -character-count/Unsolved/character-count.js
+++ 13 -character-count/Unsolved/character-count.js
@@ -2,33 +2,14 @@
 const charCount = {}
 
 var characterCount = function(str) {
-    //create empty object
-    const charCount = {}
-    const charArray = str.split("") //initial split of all of the characters
-    //for each character in the character array
-    charArray.forEach(character => {
-        //let count equal the length -1 of the initial string split split at the specific character 
-        let count = str.split(character).length - 1
-
-
-        //create or set a property with the character = key and the count = value
-        charCount[character] = count
-
-    } )
-    return charCount 
+    let charCount = {};
+    for (var i=0; i<str.length; i++) {
+        if(!charCount[str[i]]) {
+            charCount[str[i]] = 1
+        } else {
+            charCount[str[i]] = charCount[str[i]] + 1
+        }
+    }
+    return charCount
 };
 
-console.log(characterCount('Hello World!'))
-
-
-// examples of the let count = str.split(character).length-1
-var str = 'Hello World!'
-let H = str.split("H")
-let lengthH = str.split("H").length
-let countH = str.split("H").length - 1
-console.log(H, lengthH, countH) // [ '', 'ello World!' ] 2 1
-
-let l = str.split("l")
-let lengthL = str.split("l").length
-let countL = str.split("l").length - 1
-console.log(l, lengthL, countL) //[ 'He', '', 'o Wor', 'd!' ] 4 3
